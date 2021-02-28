f9df0f81de00774397b6ad03a77c4d8089654ae0
kothawleprem <kothawleprem@gmail.com>
2021-02-28 22:30:39 +0530

    Fetch from DB

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from . models import Todolist
 # Create your views here.
 
 def index(request):
-    return render(request,'myapp/index.html')
+    todo_items = Todolist.objects.order_by('id')
+    context = {
+        'todo_items' : todo_items
+        }
+    return render(request,'myapp/index.html',context)
